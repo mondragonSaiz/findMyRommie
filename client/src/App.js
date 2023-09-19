ed7cdcb09646c3c687b6915276de2c73daa036ca
David Mondragon <dragon18saiz@gmail.com>
2023-09-19 14:36:37 -0600

    Header (navbar) with responsive design - Client

--- client/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="bg-red-400">hola</div>;
-}
-
-export default App;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Home() {
+  return <section className="bg-slate-300 h-[90vh]">Homepage</section>;
+}
