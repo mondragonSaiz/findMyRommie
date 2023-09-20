895f350d3f3a1d1453aa3c5790b5b51d09af3a96
David Mondragon <dragon18saiz@gmail.com>
2023-09-19 21:57:52 -0600

    style: changed logo, added style on hover on nav links, added google font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import Header from './components/Header';
 import Home from './components/Home';
+
 function App() {
   return (
     <div className="">
       <Header />
       <Home />
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Mooli&display=swap');
+      </style>
     </div>
   );
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Home() {
-  return <section className="bg-slate-300 h-[90vh]">Homepage</section>;
+  return <section className="bg-slate-300 h-[90vh]"></section>;
 }
